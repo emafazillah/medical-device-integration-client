7dcb736c51c8c3dd97fbae520cb2d95a30150b47
Ema Fazillah Abu Bakar <emafazillah@heitech.com.my>
2017-03-30 21:24:14 +0800

    Update server.js, index.html, indexController.js

--- public/controllers/indexController.js
+++ public/controllers/indexController.js
@@ -3,7 +3,7 @@ indexApp.controller('IndexCtrl', ['$scope', '$http', function($scope, $http) {
 	console.log("Hello from indexController");
 	
 	var refresh = function() {
-		$http.get('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms/listpatientid/95746').then(function(response){
+		$http.get('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatienthiskkms/listpatientid/95746').then(function(response){
 			console.log("I GET from RESTful API");		
 			var vsms = response.data;
 			$scope.vsms = vsms;
@@ -15,7 +15,7 @@ indexApp.controller('IndexCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.add = function() {
 		console.log($scope.vsm);
 		
-		$http.post('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms/listpatientid/95746', $scope.vsm).then(function(response) {
+		$http.post('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatienthiskkms', $scope.vsm).then(function(response) {
 			console.log(response);
 			refresh();
 		});
@@ -24,7 +24,7 @@ indexApp.controller('IndexCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.remove = function(id) {
 		console.log(id);
 		
-		$http.delete('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms/' + id).then(function(response) {
+		$http.delete('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatienthiskkms/' + id).then(function(response) {
 			refresh();
 		});
 	};
@@ -32,7 +32,7 @@ indexApp.controller('IndexCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.edit = function(id) {
 		console.log(id);
 		
-		$http.get('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms/' + id).then(function(response) {
+		$http.get('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatienthiskkms/' + id).then(function(response) {
 			$scope.vsm = response.data;
 		});
 	};
@@ -40,13 +40,40 @@ indexApp.controller('IndexCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.update = function() {
 		console.log($scope.vsm.id);
 		
-		$http.put('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms/' + $scope.vsm.id, $scope.vsm).then(function(response) {
+		$http.put('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatienthiskkms/' + $scope.vsm.id, $scope.vsm).then(function(response) {
 			refresh();
 		});
 	};
 	
 	$scope.clear = function() {
 		$scope.vsm = null;
+	};
+	
+	// DEVICE list -> http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms/listpatientid/95746
+	// DEVICE latest reading -> http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms/patientid/95746
+	$scope.device = function() {
+		$http.get('http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms/patientid/95746').then(function(response) {
+			console.log("I GET request data from DEVICE");
+			
+			var deviceArray = response.data;			
+			$scope.vsm = {
+					bmi: deviceArray.bmi,
+					clinicianid: deviceArray.clinicianid,
+					date: deviceArray.date,
+					diastolic: deviceArray.diastolic,
+					height: deviceArray.height,
+					hr: deviceArray.hr,
+					map: deviceArray.map,
+					o2sat: deviceArray.o2sat,
+					pain: deviceArray.pain,
+					patientid: deviceArray.patientid,
+					pulse: deviceArray.pulse,
+					respiration: deviceArray.respiration,
+					systolic: deviceArray.systolic,
+					temperature: deviceArray.temperature,
+					weight: deviceArray.weight
+			};
+		});
 	};
 	
 }]);
--- server.js
+++ server.js
@@ -3,11 +3,99 @@ var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('vsmlist', ['vsmlist']);
 var bodyParser = require('body-parser');
+var http = require('http');
+var request = require('request-promise');
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-/* vsm */
+/* Get Data */
+function getDevice(options, callback){
+	http.request(options, function(response){		
+		var body = '';		
+		response.on('data', function(chunk){
+			body+=chunk;
+		});		
+		response.on('end', function(){	
+			var result = JSON.parse(body);
+			callback(null, result);
+		});		
+		response.on('error', callback);		
+	})
+	.on('error', callback)
+	.end();
+}
+
+var options = {
+		host: '127.0.0.1',
+		port: 9233,
+		path: '/WelchAllyn/Device/GetDevices',
+		method: 'GET'
+};
+
+/* Run every 5 seconds */
+function five() {
+	
+	/* Start Process */
+	getDevice(options, function(err, result){
+		if(err || result === null || result === undefined){
+			return console.log('Error while trying to get device', err);
+		} else {
+			console.log(result[0].deviceid);
+			
+			// post device data
+			var apiDeviceOptions = {
+					method: 'POST',
+					uri: 'http://mywildflyrestv2-emafazillah.rhcloud.com/api/tbldevices',
+					body: JSON.stringify(result[0]),
+					headers: { 'Content-Type': 'application/json' }
+			};
+			request(apiDeviceOptions).then(function(response){
+				console.log('POST Device response', response);
+			}).catch(function(err){
+				console.log('ERROR POST Device response', err);
+			});
+			
+			// get patient data
+			var currentReading = '/WelchAllyn/Device/GetCurrentReading?deviceid=' + result[0].deviceid;
+			var currentReadingOptions = {
+					host: '127.0.0.1',
+					port: 9233,
+					path: currentReading,
+					method: 'GET'
+			};
+			getDevice(currentReadingOptions, function(err, currentReadingOptionsResult){
+				if(err || currentReadingOptionsResult === null || currentReadingOptionsResult === undefined){
+					return console.log('Error while trying to get current reading', err);
+				} else {
+					console.log(currentReadingOptionsResult[0].date);
+					
+					// post current reading
+					var reconstruct = currentReadingOptionsResult[0];
+					var apiCurrentReading = {
+							method: 'POST',
+							uri: 'http://mywildflyrestv2-emafazillah.rhcloud.com/api/tblpatientvsms',
+							body: JSON.stringify(reconstruct),
+							headers: { 'Content-Type': 'application/json' }
+					};
+					request(apiCurrentReading).then(function(response){
+						console.log('POST Current Reading response', response);
+					}).catch(function(err){
+						console.log('ERROR POST Current Reading response', err);
+					});
+				}
+			});
+		}
+	});
+	
+    setTimeout(five, 5000);
+    
+}
+
+five();
+/* Get Data */
+
+/* vsm.html */
 // GET list
 app.get('/vsmlist', function(req, res) {
 	console.log("I GET response data");
@@ -74,7 +162,7 @@ app.put('/vsmlist/:id', function(req, res) {
 		res.json(doc);
 	});
 });
-/* vsm */
+/* vsm.html */
 
 app.listen(3000);
 console.log("Server is running on port 3000");
